e336e1c02150f314b01f30e9d65b81d76a0f054f
Dominic Kühne <dominic.kuehne@stud.h-da.de>
2015-06-16 11:44:33 +0200

    cowshooter update v 1.02

---
+++ src/CowshooterListener.java
@@ -0,0 +1,7 @@
+import org.bukkit.event.Listener;
+
+/**
+ * Created by Tino on 16.06.2015.
+ */
+public class CowshooterListener implements Listener {
+}
--- src/CowshooterPlugin.java
+++ src/CowshooterPlugin.java
@@ -1,5 +1,15 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
 /**
  * Created by Tino on 16.06.2015.
  */
-public class CowshooterPlugin {
+public class CowshooterPlugin extends JavaPlugin{
+    public static Logger log = Logger.getLogger("Minecraft");
+
+    public void onEnable() {
+        log.info("[CowShooter] enabling ...");
+        getServer().getPluginManager().registerEvents(new CowshooterListener(),this);
+    }
 }
